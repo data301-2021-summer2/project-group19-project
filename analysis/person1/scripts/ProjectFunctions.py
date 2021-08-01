f87a6e032ffac43790c50e7c96056d2110688a8c
Amreesa Bhabra <amreesabhabra@gmail.com>
2021-08-01 11:29:58 -0700

    trying a new function

--- analysis/person1/scripts/ProjectFunctions.py
+++ analysis/person1/scripts/ProjectFunctions.py
@@ -7,36 +7,34 @@ from pandas_profiling import ProfileReport
 
 
 
+df = pd.read_csv("../../data/raw/meteors.csv")
+df
+
 def data(path):
-#cleaned and dropped overall data  
     df2 = (
     df.rename(columns={"name":"Name given","nametype":"Classification of state","recclass": "Class","mass": "Mass", "fall": "Fall Type"})
-    .sort_values("year", ascending=True)
     .dropna()
     .drop(['reclat', 'reclong'], axis=1)
-         )
+      )
     return df2
 
 
 def DataMass(path):
-#method chain starts  - defines mass using cleaned data
-   df3 = (
-        pd.read_csv(path)
-          )
-    
-    df4= (
-        df2.sort_values(['Mass'], ascending = (True))
+    df3= (
+    df.rename(columns={"name":"Name given","nametype":"Classification of state","recclass": "Class","mass": "Mass", "fall": "Fall Type"})
+    .sort_values('Mass', ascending = True)
+    .drop(['reclat', 'reclong'], axis=1)
+    .dropna()
         )
+    return df3
+
+
+def DataYear(path): 
+    df4 = (
+    df.rename(columns={"name":"Name given","nametype":"Classification of state","recclass": "Class","mass": "Mass", "fall": "Fall Type"})
+    .sort_values("year", ascending=True)
+    .drop(['reclat', 'reclong'], axis=1)
+    .dropna()
+)
     return df4
 
-
-def DataYear(path):
- #method chain starts - defines year using cleaned data
-    df5 = (
-        pd.read_csv(path)
-          )
-    
-    df6 = (
-        df2.sort_values(['year'], ascending = (True))
-          )
-    return df6
